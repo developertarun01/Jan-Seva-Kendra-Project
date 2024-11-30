b08db8a805af4ab685b4b5f9b677a4d86314be92
developertarun01 <tarunbusinessmail@gmail.com>
2024-11-30 14:36:03 +0530

    checking

--- jan-seva-kendra-frontend/src/pages/Home.js
+++ jan-seva-kendra-frontend/src/pages/Home.js
@@ -56,7 +56,7 @@ function Home() {
         }
 
         try {
-            const response = await fetch("https://jan-seva-kendra-project-api.vercel.app/ ", {
+            const response = await fetch("http://localhost:5000/ ", {
                 method: "POST",
                 body: data,
             });
